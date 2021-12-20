a07a965acfe2d13de5e3b323229e78271e2624c3
liliang <ll917274996@live.com>
2021-12-20 20:48:42 +0800

    修改发布工作流和base

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/newui/',
   plugins: [vue()],
   build: process.env.TARGET === 'lib'
     ? {
