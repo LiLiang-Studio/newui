bb91646c9fd906491a0e1e4a11618909a72f6aa7
liliang <ll917274996@live.com>
2021-12-21 17:58:54 +0800

    增加卡片组件

---
+++ lib/components/card/Card.vue
@@ -0,0 +1,25 @@
+<template>
+  <div :class="[cls, `${cls}_${shadow}`]">
+    <div v-if="$slots.header || header" :class="`${cls}_header`">
+      <slot name="header">{{ header }}</slot>
+    </div>
+    <div :class="`${cls}_body`" :style="bodyStyle">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { oneOf, S } from '../../types'
+
+defineProps({
+  header: S,
+  bodyStyle: {},
+  shadow: {
+    default: 'always',
+    validator: v => oneOf(['always', 'hover', 'never'], v)
+  }
+})
+
+const cls = 'x-card'
+</script>
---
+++ src/views/pages/Card.vue
@@ -0,0 +1,106 @@
+<template>
+  <div>
+    <el-card class="box-card">
+      <template #header>
+        <div class="clearfix">
+          <span>卡片名称</span>
+          <el-link style="float: right;" type="primary" :underline="false">操作按钮</el-link>
+        </div>
+      </template>
+      <div v-for="o in 4" :key="o" class="text item">
+        {{'列表内容 ' + o }}
+      </div>
+    </el-card><br>
+
+    <el-card class="box-card">
+      <div v-for="o in 4" :key="o" class="text item">
+        {{'列表内容 ' + o }}
+      </div>
+    </el-card><br>
+
+    <el-row>
+      <el-col :span="8" v-for="(o, index) in 2" :key="o" :offset="index > 0 ? 2 : 0">
+        <el-card :body-style="{ padding: '0px' }">
+          <img src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png" class="image">
+          <div style="padding: 14px;">
+            <span>好吃的汉堡</span>
+            <div class="bottom clearfix">
+              <time class="time">{{ currentDate }}</time>
+              <el-link type="primary" :underline="false" class="button">操作按钮</el-link>
+            </div>
+          </div>
+        </el-card>
+      </el-col>
+    </el-row><br>
+
+    <el-row :gutter="12">
+      <el-col :span="8">
+        <el-card shadow="always">
+          总是显示
+        </el-card>
+      </el-col>
+      <el-col :span="8">
+        <el-card shadow="hover">
+          鼠标悬浮时显示
+        </el-card>
+      </el-col>
+      <el-col :span="8">
+        <el-card shadow="never">
+          从不显示
+        </el-card>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      currentDate: new Date().toLocaleString()
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .text {
+    font-size: 14px;
+  }
+
+  .item {
+    margin-bottom: 18px;
+  }
+
+  .clearfix:before,
+  .clearfix:after {
+    display: table;
+    content: "";
+  }
+  .clearfix:after {
+    clear: both
+  }
+
+  .box-card {
+    width: 480px;
+  }
+  .time {
+    font-size: 13px;
+    color: #999;
+  }
+
+  .bottom {
+    margin-top: 13px;
+    line-height: 12px;
+  }
+
+  .button {
+    padding: 0;
+    float: right;
+  }
+
+  .image {
+    width: 100%;
+    display: block;
+  }
+</style>
