89e3e71ee18247865afd9d76a5d9a7f7cba61f4a
liliang <ll917274996@live.com>
2021-12-21 11:03:47 +0800

    完善按钮组件

--- lib/components/button/Button.vue
+++ lib/components/button/Button.vue
@@ -1,3 +1,56 @@
 <template>
-  <div></div>
+  <button :class="classes" :type="nativeType" :disabled="isDisabled || loading">
+    <i v-if="loading" class="x-icon-loading"></i>
+    <i v-else-if="icon" :class="icon"></i>
+    <span v-if="$slots.default">
+      <slot />
+    </span>
+  </button>
 </template>
+
+<script setup>
+import { computed, inject } from 'vue'
+import { B, oneOf, S, statusTypes } from '../../types'
+
+const props = defineProps({
+  size: {
+    validator: v => oneOf(['medium', 'small', 'mini'], v)
+  },
+  type: {
+    default: 'default',
+    validator: v => oneOf(['default', 'primary', 'text', ...statusTypes], v)
+  },
+  plain: B,
+  round: B,
+  circle: B,
+  loading: B,
+  disabled: B,
+  icon: S,
+  nativeType: {
+    default: 'button',
+    validator: v => oneOf(['button', 'submit', 'reset'], v)
+  }
+})
+
+const xForm = inject('xForm', {})
+const xButtonGroup = inject('xButtonGroup', {})
+
+const isDisabled = computed(() => props.disabled || xForm.disabled)
+
+const classes = computed(() => {
+  const cls = 'x-button'
+  const size = props.size || xButtonGroup.size || xForm.size
+  return [
+    cls,
+    `${cls}_${props.type}`,
+    {
+      [`${cls}_${size}`]: size,
+      'is-plain': props.plain,
+      'is-loading': props.loading,
+      'is-round': props.round,
+      'is-circle': props.circle,
+      'is-disabled': isDisabled.value
+    }
+  ]
+})
+</script>
--- lib/components/button/ButtonGroup.vue
+++ lib/components/button/ButtonGroup.vue
@@ -1,3 +1,14 @@
 <template>
-  <div></div>
+  <div class="x-button-group">
+    <slot />
+  </div>
 </template>
+
+<script setup>
+import { provide, reactive } from 'vue'
+import { S } from '../../types'
+
+const props = defineProps({ size: S })
+
+provide('xButtonGroup', reactive(props))
+</script>
