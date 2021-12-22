67aad2169df4535212393a2346a9017273587288
liliang <ll917274996@live.com>
2021-12-22 11:50:39 +0800

    标签组件

---
+++ lib/components/tag/Tag.vue
@@ -0,0 +1,47 @@
+<template>
+  <transition :name="cls">
+    <span :class="classes" :style="{ backgroundColor: color }" v-bind="$attrs">
+      <slot />
+      <i v-if="closable" :class="['x-icon-close', `${cls}_close`]" @click="emit('close')"></i>
+    </span>
+  </transition>
+</template>
+
+<script setup>
+import { computed } from 'vue'
+import { B, oneOf, S, statusTypes } from '../../types'
+
+const props = defineProps({
+  type: {
+    validator: v => !v || oneOf(statusTypes, v)
+  },
+  closable: B,
+  disableTransitions: B,
+  hit: B,
+  color: S,
+  size: {
+    validator: v => oneOf(['medium', 'small', 'mini'], v)
+  },
+  effect: {
+    default: 'light',
+    validator: v => oneOf(['dark', 'light', 'plain'], v)
+  }
+})
+
+const emit = defineEmits(['close'])
+
+const cls = 'x-tag'
+
+const classes = computed(() => {
+  return [
+    cls,
+    props.type && `${cls}_${props.type}`,
+    props.size && `${cls}_${props.size}`,
+    `${cls}_${props.effect}`,
+    {
+      'is-hit': props.hit,
+      'is-fade': !props.disableTransitions
+    }
+  ]
+})
+</script>
