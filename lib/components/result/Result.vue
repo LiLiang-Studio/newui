796fa3c74487923b84d7eaf186dee7daaae3c826
liliang <ll917274996@live.com>
2021-12-22 16:38:04 +0800

    结果组件

---
+++ lib/components/result/Result.vue
@@ -0,0 +1,33 @@
+<template>
+  <div :class="cls">
+    <div :class="`${cls}_icon`">
+      <slot name="icon">
+        <i :class="[`icon-${icon}`, `x-icon-${icon}`]"></i>
+      </slot>
+    </div>
+    <div :class="`${cls}_title`">
+      <slot name="title">{{ title }}</slot>
+    </div>
+    <div v-if="$slots.subTitle || subTitle" :class="`${cls}_subtitle`">
+      <slot name="subTitle">{{ subTitle }}</slot>
+    </div>
+    <div v-if="$slots.extra" :class="`${cls}_extra`">
+      <slot name="extra"></slot>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { oneOf, S } from '../../types'
+
+defineProps({
+  title: S,
+  subTitle: S,
+  icon: {
+    default: 'info',
+    validator: v => oneOf(['success', 'warning', 'info', 'error'], v)
+  }
+})
+
+const cls = 'x-result'
+</script>
