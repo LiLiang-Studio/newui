ea4b28b639e53b021251e7339c422af4c282e771
liliang <ll917274996@live.com>
2021-12-23 16:52:41 +0800

    时间线组件

--- lib/components/timeline/Timeline.vue
+++ lib/components/timeline/Timeline.vue
@@ -1,11 +1,16 @@
-<template>
-  <ul class="x-timeline">
-    <slot />
-  </ul>
-</template>
-
-<script setup>
-import { B } from '../../types'
-
-const props = defineProps({ reverse: B })
+<script>
+import { h } from 'vue'
+import { B, isArr } from '../../types'
+export default {
+  props: {
+    reverse: B
+  },
+  render () {
+    const vnodes = this.$slots.default()
+    this.reverse && vnodes.forEach(_ => {
+      isArr(_.children) && _.children.reverse()
+    })
+    return h('ul', { class: 'x-timeline' }, vnodes)
+  }
+}
 </script>
