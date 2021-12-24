ea7cb70316524753bff88f3082e5079cc0d0bede
liliang <ll917274996@live.com>
2021-12-24 11:10:50 +0800

    新建通知组件

---
+++ lib/components/notification/Notification.vue
@@ -0,0 +1,21 @@
+<template>
+  <div></div>
+</template>
+
+<script setup>
+import { BTrue, N, oneOf, S } from '../../types'
+
+const props = defineProps({
+  title: S,
+  type: {
+    validator: v => oneOf(['success', 'warning', 'info', 'error'], v)
+  },
+  iconClass: S,
+  customClass: S,
+  duration: {
+    type: N,
+    default: 4500
+  },
+  showClose: BTrue
+})
+</script>
