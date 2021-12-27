3121fe5f43a2dfd2f1b2a46bbad10e961559c834
liliang <ll917274996@live.com>
2021-12-27 12:17:31 +0800

    完善loading组件

---
+++ lib/components/loading/Loading.vue
@@ -0,0 +1,28 @@
+<template>
+  <div :class="[cls, customClass]" :style="{}">
+    <div :class="`${cls}_spinner`">
+      <i v-if="spinner" :class="spinner"></i>
+      <svg v-else viewBox="25 25 50 50" :class="`${cls}_circular`">
+        <circle :class="`${cls}_path`" cx="50" cy="50" r="20" fill="none"></circle>
+      </svg>
+      <p :class="`${cls}_text`">{{ text }}</p>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { B, BTrue, O, S } from '../../types'
+
+const props = defineProps({
+  target: [O, S],
+  body: B,
+  fullscreen: BTrue,
+  lock: B,
+  text: S,
+  spinner: S,
+  background: S,
+  customClass: S
+})
+
+const cls = 'x-loading'
+</script>
---
+++ src/views/pages/Loading.vue
@@ -0,0 +1,71 @@
+<template>
+  <div>
+    <div v-loading class="area">
+      这是一些内容这是一些内容这是一些内容这是一些内容这是一些内容这是一些内容这是一些内容这是一些内容这是一些内容这是一些内容这是一些内容
+    </div>
+    <div
+      class="area"
+      v-loading="loading"
+      element-loading-text="拼命加载中"
+      element-loading-spinner="el-icon-loading"
+      element-loading-background="rgba(0, 0, 0, 0.8)"
+    >这是一些内容这是一些内容这是一些内容这是一些内容这是一些内容这是一些内容这是一些内容这是一些内容这是一些内容这是一些内容这是一些内容</div>
+    <el-button
+      type="primary"
+      @click="openFullScreen1"
+      v-loading.fullscreen.lock="fullscreenLoading">
+      指令方式
+    </el-button>
+    <el-button
+      type="primary"
+      @click="openFullScreen2">
+      服务方式
+    </el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      loading: true,
+      fullscreenLoading: false
+    }
+  },
+  methods: {
+    openFullScreen1() {
+      this.fullscreenLoading = true;
+      setTimeout(() => {
+        this.fullscreenLoading = false;
+      }, 2000);
+    },
+    openFullScreen2() {
+      const loading = this.$loading({
+        lock: true,
+        text: 'Loading',
+        spinner: 'el-icon-loading',
+        background: 'rgba(0, 0, 0, 0.7)'
+      });
+      setTimeout(() => {
+        loading.close();
+      }, 2000);
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.area {
+  height: 200px;
+  border: 1px solid #eee;
+  border-radius: 6px;
+  margin-bottom: 20px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 20px;
+}
+.x-button {
+  margin-right: 10px;
+}
+</style>
